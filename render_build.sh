dcd4e5c306f663f1a2d6442891829daf875e545f
Sing Sation Global <singsationglobal@gmail.com>
2026-09-16 22:34:21 +0200

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 set -e
 
-echo "Installing Flutter..."
-git clone https://github.com/flutter/flutter.git -b stable --depth 1
-export PATH="$PATH:$(pwd)/flutter/bin"
+FLUTTER_DIR="$HOME/flutter"
+
+if [ ! -d "$FLUTTER_DIR" ]; then
+  echo "Installing Flutter..."
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1 "$FLUTTER_DIR"
+fi
+
+export PATH="$PATH:$FLUTTER_DIR/bin"
 
 echo "Flutter version:"
 flutter --version
+
+echo "Cleaning previous build artifacts..."
+flutter clean
+
+echo "Getting packages..."
+flutter pub get
 
 echo "Building web app..."
 flutter build web --release
